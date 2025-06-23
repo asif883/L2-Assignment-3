6ff86faecf08ae55681da54178aa7d49859903d9
asif883 <asifiqbal95863@gmail.com>
2025-06-23 20:10:06 +0600

    ready to deploy

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+  ],
+  files: ['**/*.ts'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+});
